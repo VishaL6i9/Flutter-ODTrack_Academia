c0c8cc5802f13ad1f1ce5199aeed3c02b6b6cf87
VishaL6i9 <vishalkandakatla@gmail.com>
2025-08-17 12:35:01 +0530

    feat: Added accent color to theme

--- lib/core/theme/app_theme.dart
+++ lib/core/theme/app_theme.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 class AppTheme {
   // Color Palette
   static const Color primaryColor = Color(0xFF1976D2);
+  static const Color accentColor = Color(0xFF03DAC6);
   static const Color secondaryColor = Color(0xFF03DAC6);
   static const Color errorColor = Color(0xFFB00020);
   static const Color successColor = Color(0xFF4CAF50);
