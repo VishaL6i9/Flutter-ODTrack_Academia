b37d4514b7554f710c3d94bec4ed0d6b8ffbe5ac
VishaL6i9 <vishalkandakatla@gmail.com>
2026-03-10 04:18:03 +0530

    feat(core): add period slot model

--- lib/models/period_slot.dart
+++ lib/models/period_slot.dart
@@ -1,6 +1,27 @@
+import 'package:json_annotation/json_annotation.dart';
+import 'package:hive/hive.dart';
+
+part 'period_slot.g.dart';
+
+@HiveType(typeId: 5)
+@JsonSerializable()
 class PeriodSlot {
+  @HiveField(0)
   final String subject;
+
+  @HiveField(1)
   final String? staffId;
 
-  const PeriodSlot({required this.subject, this.staffId});
+  @HiveField(2)
+  final String? type;
+
+  const PeriodSlot({
+    required this.subject,
+    this.staffId,
+    this.type,
+  });
+
+  factory PeriodSlot.fromJson(Map<String, dynamic> json) => _$PeriodSlotFromJson(json);
+
+  Map<String, dynamic> toJson() => _$PeriodSlotToJson(this);
 }
